465a8ef12e8a97bcdd351b7b39bcd1aceccc05cf
Julia Gong <juliaxgong@gmail.com>
2018-12-07 14:58:06 -0800

    Moved new search over to search_phonememod.py file.

--- scripts/search_phonememod.py
+++ scripts/search_phonememod.py
@@ -33,11 +33,12 @@ def find_closest_pinyin(name, do_print):
         cost_keep, s_keep = recurse(syllables_keep, rest[1:])
         syllables[-1] = syllables[-1][:-1]
 
-        #if syllables[0] != "":
-        syllables.append(rest[0])
-        syllables_end = [x for x in syllables]
-        cost_end, s_end = recurse(syllables_end, rest[1:])
-        syllables = syllables[:-1]
+        cost_end, s_end = (0, [])
+        if syllables[0] != "":
+            syllables.append(rest[0])
+            syllables_end = [x for x in syllables]
+            cost_end, s_end = recurse(syllables_end, rest[1:])
+            syllables = syllables[:-1]
 
         result = min((cost_end, s_end), (cost_keep, s_keep))
         cache[hash_sylls] = result
@@ -49,18 +50,19 @@ def find_closest_pinyin(name, do_print):
     return recurse([""], name)
     #return all_actions
 
-def evaluate_preidctions():
+def evaluate_predictions():
     all_names = search_utils.chinese_names
     distance = 0
     diff_count = 0
     for row_i, row in all_names.iterrows():
         if row_i % 20 == 0:
-            print("{.2f}% complete".format(100 * row_i/all_names.shape[0]))
+            print("{}% complete".format(100 * row_i/all_names.shape[0]))
         english, _, _, target_pinyin = row
         english = search_utils.normalize(english)
+        english = search_utils.phoneme_adjust(english)
         target_pinyin = ''.join(filter(lambda x: x != ' ', search_utils.normalize(target_pinyin)))
 
-        output_pinyin = ''.join(find_closest_pinyin(english)[1])
+        output_pinyin = ''.join(find_closest_pinyin(english, False)[1])
         if output_pinyin != target_pinyin:
             #print (english, output_pinyin, target_pinyin)
             diff_count += 1
@@ -74,10 +76,11 @@ def evaluate_preidctions():
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == 'eval':
-        evaluate_preidctions()
+        evaluate_predictions()
     else:
         while True:
             name = input("name >> ")
-            result = find_closest_pinyin(name)
-            print(resul
+            name = search_utils.phoneme_adjust(name)
+            result = find_closest_pinyin(name, True)
+            print(result)
 
